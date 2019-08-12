1098b92266a692597e598c2cde97c8d5559f944f
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-11 20:11:44 -0400

    responsive to smaller phone sizes

--- app/javascript/react/containers/LandingPage.js
+++ app/javascript/react/containers/LandingPage.js
@@ -9,35 +9,41 @@ class LandingPage extends Component {
   render() {
     return(
       <div className="row">
+      
         <div className="span-image small-12 columns">
           <img src="/lich.jpg" className="landingImage"/>
         </div>
-        <div className="textOverImage small-10 small-centered columns">
+        
+        <div className="textOverImage small-10 medium-10 small-centered columns">
           <div className="row">
-            <h1 className="pageTitle">Magic Arena <br />
-            <span className="pageSubTitle">Draft Helper</span></h1>
+            <h1 className="pageTitle">Magic Arena</h1>
+            <h1 id="pageSubTitle">Draft Helper</h1>
           </div>  
           <div className="row">
-            <div className="small-4 columns">
+            <div className="small-9 small-centered medium-4 medium-uncentered columns">
               <h2>Upload a screenshot of your draft pack, and the helper will tell you which are your strongest picks!</h2>
             </div>
           </div>
           <div className="row">  
-            <div className="small-2 columns">
+            <div className="small-10 small-centered medium-2 medium-uncentered columns">
               <a href="/drafts"><button className='btnTryNow'>Try Now!</button></a>
             </div>
           </div>
         </div>
+        
         <div className="row">
           <div className="small-10 small-centered columns">
-            <div className="small-4 columns landingPageText">
+          
+            <div className="small-10 small-centered medium-4 medium-uncentered columns landingPageText">
               <p>Simply open a draft pack in Magic Arena, upload a screenshot of it to the helper, and it will return your cards sorted by their ratings according to several different tier lists! Filter and sort results by color and cost.</p>
             </div>
-            <div className="small-8 columns">
+            <div className="small-10 small-centered medium-8 medium-uncentered columns">
               <img src="/LandingPageGraphic.png" className="landingPageIcons"/>
             </div>
+            
           </div>
         </div>
+        
       </div>
     )
   }
